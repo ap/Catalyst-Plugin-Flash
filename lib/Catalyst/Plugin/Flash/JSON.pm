e47dd6e001a7a33334519785d96fb4e4fdeac508
Aristotle Pagaltzis <pagaltzis@gmx.de>
2024-07-07 10:10:13 +0200

    release 0.002

--- lib/Catalyst/Plugin/Flash/JSON.pm
+++ lib/Catalyst/Plugin/Flash/JSON.pm
@@ -3,7 +3,7 @@ use 5.008001; use strict; use warnings;
 package # hide from PAUSE for now
 	Catalyst::Plugin::Flash::JSON;
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 use JSON::MaybeXS;
 my $jx = JSON::MaybeXS->new->ascii;
